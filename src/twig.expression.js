b6030670c0e63a504faee30bb14de032468e32ac
John Roepke <john@justjohn.us>
2011-10-08 00:12:59 +0000

    Move the expression compiling methods into the definitions.

--- src/twig.expression.js
+++ src/twig.expression.js
@@ -51,7 +51,17 @@ var Twig = (function (Twig) {
                 Twig.expression.type.number,
                 Twig.expression.type.array.start,
                 Twig.expression.type.object.start
-            ]
+            ],
+            compile: function(token, stack, output) {
+                while(stack.length > 0) {
+                    output.push(stack.pop());
+                }
+                output.push(token);
+                return {
+                    stack: stack,
+                    output: output
+                };
+            }
         },
         {
             type: Twig.expression.type.expression,
@@ -60,7 +70,18 @@ var Twig = (function (Twig) {
             next: [
                 Twig.expression.type.operator,
                 Twig.expression.type.array.end
-            ]
+            ],
+            compile: function(token, stack, output) {
+                var evaluated_expression = Twig.expression.compile(token),
+                    sub_stack = evaluated_expression.stack;
+                while (sub_stack.length > 0) {
+                    output.push(sub_stack.shift());
+                }
+                return {
+                    stack: stack,
+                    output: output
+                };
+            }
         },
         {
             type: Twig.expression.type.operator,
@@ -73,7 +94,29 @@ var Twig = (function (Twig) {
                 Twig.expression.type.number,
                 Twig.expression.type.array.start,
                 Twig.expression.type.object.start
-            ]
+            ],
+            compile: function(token, stack, output) {
+                var value = token.value,
+                    operator = Twig.expression.operator.lookup(value, token);
+                    
+                Twig.log.trace("Twig.expression.compile: ", "Operator: ", operator);
+
+                while (stack.length > 0 && (
+                            (operator.associativity === Twig.expression.operator.leftToRight &&
+                             operator.precidence    >= stack[stack.length-1].precidence) ||
+
+                            (operator.associativity === Twig.expression.operator.rightToLeft &&
+                             operator.precidence    >  stack[stack.length-1].precidence))
+                       ) {
+                     output.push(stack.pop());
+                }
+                stack.push(operator);
+                
+                return {
+                    stack: stack,
+                    output: output
+                };
+            }
         },
         {
             /**
@@ -86,7 +129,24 @@ var Twig = (function (Twig) {
                 Twig.expression.type.operator,
                 Twig.expression.type.array.end,
                 Twig.expression.type.comma
-            ]
+            ],
+            compile: function(token, stack, output) {
+                var value = token.value;
+                
+                // Remove the quotes from the string
+                if (value.substring(0, 1) === '"') {
+                    value = value.replace('\\"', '"');
+                } else {
+                    value = value.replace("\\'", "'");
+                }
+                token.value = value.substring(1, value.length-1);
+                Twig.log.trace("Twig.expression.compile: ", "String value: ", token.value);
+                output.push(token);
+                return {
+                    stack: stack,
+                    output: output
+                };
+            }
         },
         {
             /**
@@ -100,7 +160,14 @@ var Twig = (function (Twig) {
                 Twig.expression.type.variable,
                 Twig.expression.type.number,
                 Twig.expression.type.array.end
-            ]
+            ],
+            compile: function(token, stack, output) {
+                output.push(token);
+                return {
+                    stack: stack,
+                    output: output
+                };
+            }
         },
         {
             /**
@@ -112,7 +179,17 @@ var Twig = (function (Twig) {
                 Twig.expression.type.filter,
                 Twig.expression.type.array.end,
                 Twig.expression.type.comma
-            ]
+            ],
+            compile: function(token, stack, output) {
+                while(stack.length > 0) {
+                    output.push(stack.pop());
+                }
+                output.push(token);
+                return {
+                    stack: stack,
+                    output: output
+                };
+            }
         },
         {
             /**
@@ -168,7 +245,14 @@ var Twig = (function (Twig) {
                 Twig.expression.type.filter,
                 Twig.expression.type.array.end,
                 Twig.expression.type.comma
-            ]
+            ],
+            compile: function(token, stack, output) {
+                output.push(token);
+                return {
+                    stack: stack,
+                    output: output
+                };
+            }
         },
         {
             /**
@@ -182,9 +266,45 @@ var Twig = (function (Twig) {
                 Twig.expression.type.filter,
                 Twig.expression.type.array.end,
                 Twig.expression.type.comma
-            ]
+            ],
+            compile: function(token, stack, output) {
+                output.push(token);
+                return {
+                    stack: stack,
+                    output: output
+                };
+            }
         }
     ];
+
+    /**
+     * Registry for logic handlers.
+     */
+    Twig.expression.handler = {};
+
+    /**
+     * Define a new expression type, available at Twig.logic.type.{type}
+     */
+    Twig.expression.extendType = function (type, value) {
+        value = value || type;
+        Twig.expression.type[type] = value;
+    };
+
+    /**
+     * Extend the expression parsing functionality with a new definition.
+     */
+    Twig.expression.extend = function (definition) {
+
+        if (!definition.type) {
+            throw "Unable to extend logic definition. No type provided for " + definition;
+        }
+        Twig.expression.handler[definition.type] = definition;
+    };
+
+    // Extend with built-in expressions
+    while (Twig.expression.definitions.length > 0) {
+        Twig.expression.extend(Twig.expression.definitions.shift());
+    }
 
     /**
      * Break an expression into tokens defined in Twig.expression.definitions.
@@ -205,8 +325,6 @@ var Twig = (function (Twig) {
             next,
             // Has a match been found from the definitions
             match_found,
-            i,
-            l,
             match_function = function (match) {
                 Twig.log.trace("Twig.expression.tokenize",
                                "Matched a ", type, " regular expression of ", match);
@@ -243,19 +361,19 @@ var Twig = (function (Twig) {
 
         while (expression.length > 0) {
             expression = expression.trim();
-            l = Twig.expression.definitions.length;
-            for (i = 0; i < l; i += 1) {
-                next = Twig.expression.definitions[i].next;
-                type = Twig.expression.definitions[i].type;
-                regex = Twig.expression.definitions[i].regex;
-                match_found = false;
-
-                expression = expression.replace(regex, match_function);
-
-                // An expression token has been matched. Break the for loop and start trying to
-                //  match the next template (if expression isn't empty.)
-                if (match_found) {
-                    break;
+            for (type in Twig.expression.handler) {
+                if (Twig.expression.handler.hasOwnProperty(type)) {
+                    next = Twig.expression.handler[type].next;
+                    regex = Twig.expression.handler[type].regex;
+                    match_found = false;
+    
+                    expression = expression.replace(regex, match_function);
+    
+                    // An expression token has been matched. Break the for loop and start trying to
+                    //  match the next template (if expression isn't empty.)
+                    if (match_found) {
+                        break;
+                    }
                 }
             }
             if (!match_found) {
@@ -271,10 +389,11 @@ var Twig = (function (Twig) {
         var expression = raw_token.value,
             // Tokenize expression
             tokens = Twig.expression.tokenize(expression),
-            output = [],
-            operator_stack = [],
             token = null,
-            value = null;
+            output = [],
+            stack = [],
+            value = null,
+            token_template = null;
 
         Twig.log.trace("Twig.expression.compile: ", "Compiling ", expression);
         Twig.log.trace("Twig.expression.compile: ", "Tokens tokenized to ", tokens);
@@ -284,84 +403,16 @@ var Twig = (function (Twig) {
 
         while (tokens.length > 0) {
             token = tokens.shift();
-            value = token.value;
-
-            switch (token.type) {
-                // variable/contant types
-                case Twig.expression.type.string:
-                    // Remove the quotes from the string
-                    if (value.substring(0,1) === '"') {
-                        value = value.replace('\\"', '"');
-                    } else {
-                        value = value.replace("\\'", "'");
-                    }
-                    token.value = value.substring(1, value.length-1);
-                    Twig.log.trace("Twig.expression.compile: ", "String value: ", token.value);
-                    output.push(token);
-                    break;
-
-                case Twig.expression.type.variable:
-                case Twig.expression.type.number:
-                    Twig.log.trace("Twig.expression.compile: ", "Var/number value: ", value);
-                    output.push(token);
-                    break;
-
-
-                /**
-                 * Handler operators (e.g. +,-,/,etc...)
-                 *
-                 * This looks up the operator in the
-                 */
-                case Twig.expression.type.operator:
-                    var operator = Twig.expression.operator.lookup(value, token);
-                    Twig.log.trace("Twig.expression.compile: ", "Operator: ", operator);
-
-                    while (operator_stack.length > 0 && (
-                                (operator.associativity === Twig.expression.operator.leftToRight &&
-                                 operator.precidence    >= operator_stack[operator_stack.length-1].precidence) ||
-
-                                (operator.associativity === Twig.expression.operator.rightToLeft &&
-                                 operator.precidence    >  operator_stack[operator_stack.length-1].precidence))
-                           ) {
-                         output.push(operator_stack.pop());
-                    }
-
-                    operator_stack.push(operator);
-                    break;
-
-                /**
-                 * Handle sub-expressions (expressions in parenthesis)
-                 */
-                case Twig.expression.type.expression:
-                    var evaluated_expression = Twig.expression.compile(token),
-                        sub_stack = evaluated_expression.stack;
-                    while (sub_stack.length > 0) {
-                        output.push(sub_stack.shift());
-                    }
-                    break;
-
-                case Twig.expression.type.comma:
-                case Twig.expression.type.array.end:
-                    while(operator_stack.length > 0) {
-                        output.push(operator_stack.pop());
-                    }
-                    output.push(token);
-                    break;
-                    
-                case Twig.expression.type.object.start:
-                case Twig.expression.type.object.end:
-                case Twig.expression.type.array.start:
-                    output.push(token);
-                    break;
-
-                case Twig.expression.type.filter:
-                    // TODO: implement
-                    break;
+            token_template = Twig.expression.handler[token.type];
+            if (token_template.compile) {
+                value = token_template.compile(token, stack, output);
+                output = value.output && output;
+                stack = value.stack && stack;
             }
         }
 
-        while(operator_stack.length > 0) {
-            output.push(operator_stack.pop());
+        while(stack.length > 0) {
+            output.push(stack.pop());
         }
 
         Twig.log.trace("Twig.expression.compile: ", "Stack is", output);
