35e7a13b86ff136e31b5e4fe2a8b2fbdd51075d6
John Roepke <john@justjohn.us>
2011-10-12 23:58:14 +0000

    Added shell script for combining all the js files into a single file

---
+++ tools/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Combine the js files into a single file
+INPUT="twig.js twig.fills.js twig.logic.js twig.expression.js twig.expression.operator.js"
+
+# Write the combined output to this file
+OUTPUT="twig.dev.js"
+
+# assume this script is running from the scripts, src or root directory
+if [ -f twig.js ]; then
+    BASE=""
+elif [ -f src/twig.js ]; then
+	BASE="src"
+else
+	BASE="../src"
+fi
+
+echo "" > $BASE/$OUTPUT
+
+for file in $INPUT; do
+	cat $BASE/$file >> $BASE/$OUTPUT
+	echo >> $BASE/$OUTPUT
+done
+
