c85d3ebee0d6b22adc937714d0bfcc0c3d82ea0a
John Roepke <john@justjohn.us>
2011-10-14 00:37:38 -0400

    Added combined version of twig.js, added license headers to module files

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-// Load twig
+/**
+ * Twig.js v0.1
+ * Copyright (c) 2011 John Roepke
+ * Available under the BSD 2-Clause License
+ */
+
 module.exports = require("./src/twig.dev");
 
--- src/twig.module.js
+++ src/twig.module.js
@@ -1,3 +1,9 @@
+/**
+ * Twig.js v0.1
+ * Copyright (c) 2011 John Roepke
+ * Available under the BSD 2-Clause License
+ */
+
 // Provide a CommonJS module export.
 if (typeof module !== undefined && module.exports) {
     module.exports = twig;
