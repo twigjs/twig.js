d841ca6d904c77d2cd764d47064ebdece15ce5bc
John Roepke <john@justjohn.us>
2011-12-21 04:11:44 -0500

    Update the node/express demo to be a little more complicated.
    --HG--
    rename : demos/node_express/views/layout.twig => demos/node_express/public/views/layout.twig
    rename : demos/node_express/views/pages/index.twig => demos/node_express/public/views/pages/index.twig

--- demos/node_express/app.js
+++ demos/node_express/app.js
@@ -1,7 +1,21 @@
-var twig = require("../../twig"),
-    app = require('express').createServer();
+var twig = require("../../twig")
+    , express = require('express')
+    , app = express.createServer();
+
+var notes = {
+    1: {
+        title: "Notes"
+        , text: "These could be your notes. But you would have to turn this demo program into something beautiful."
+    }
+    , 2: {
+        title: "Templates"
+        , text: "Templates are a way of enhancing content with markup. Or really anything that requires the merging of data and display."
+    }
+};
 
 app.configure(function () {
+    app.use(express.static(__dirname + '/public'));
+    app.set('views', __dirname + '/public/views');
     app.set('view engine', 'twig');
     // We don't need express to use a parent "page" layout
     app.set("view options", { layout: false });
@@ -14,6 +28,33 @@ app.get('/', function(req, res){
   res.render('pages/index', {
     message : "Hello World"
   });
+});
+
+app.get('/notes', function(req, res) {
+  var json = req.param("json");
+
+  if (json) {
+    res.json({
+      notes : notes
+    });
+  } else {
+    res.render('pages/notes', {
+      notes : notes
+    });
+  }
+});
+
+
+app.get('/notes/:id', function(req, res) {
+  var json = req.param("json");
+  var id = parseInt(req.params.id);
+  var note = notes[id];
+
+  if (json) {
+    res.json(note);
+  } else {
+    res.render('pages/note', note);
+  }
 });
 
 app.listen(9999);
