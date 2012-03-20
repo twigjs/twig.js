675463ef296c7449adda82197ae68c74c147aa17
John Roepke <john@justjohn.us>
2012-03-20 09:22:22 -0400

    Cleanup of compiled test templates, fix typo in usage for twigjs.

--- test/compiler/templates/sub/sub.twig.js
+++ test/compiler/templates/sub/sub.twig.js
@@ -1,1 +1,5 @@
-twig({id:"templates/sub/sub.twig", data:[{"type":"raw","value":"I'm a "},{"type":"output","stack":[{"type":"Twig.expression.type.variable","value":"type","match":["type"]}]},{"type":"raw","value":"\n"}], precompiled: true});
+module.declare([{ twig: "vendor/twig" }], function (require, exports, module) {
+	var twig = require("twig").twig;
+	exports.template = twig({id:"templates/sub/sub.twig", data:[{"type":"raw","value":"I'm a "},{"type":"output","stack":[{"type":"Twig.expression.type.variable","value":"type","match":["type"]}]},{"type":"raw","value":"\n"}], precompiled: true});
+
+});
--- test/compiler/templates/test.twig.js
+++ test/compiler/templates/test.twig.js
@@ -1,5 +1,1 @@
-module.declare([{ twig: "undefined" }], function (require, exports, module) {
-	var twig = require("twig").twig;
-	exports.template = twig({id:"templates/test.twig", data:[{"type":"logic","token":{"type":"Twig.logic.type.if","stack":[{"type":"Twig.expression.type.variable","value":"test","match":["test"]}],"output":[{"type":"raw","value":"\n    Yep\n"}]}},{"type":"logic","token":{"type":"Twig.logic.type.else","match":["else"],"output":[{"type":"raw","value":"\n    Nope\n"}]}},{"type":"raw","value":"\n"}], precompiled: true});
-
-});
+twig({id:"templates/test.twig", data:[{"type":"logic","token":{"type":"Twig.logic.type.if","stack":[{"type":"Twig.expression.type.variable","value":"test","match":["test"]}],"output":[{"type":"raw","value":"\n    Yep\n"}]}},{"type":"logic","token":{"type":"Twig.logic.type.else","match":["else"],"output":[{"type":"raw","value":"\n    Nope\n"}]}},{"type":"raw","value":"\n"}], precompiled: true});
