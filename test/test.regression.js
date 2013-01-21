0d0edf37bd7fddf0c5457dfa3521c6a7b8d0dc79
John Roepke <john@justjohn.us>
2013-01-21 00:01:36 -0500

    Add tests for issue #56

--- test/test.regression.js
+++ test/test.regression.js
@@ -6,5 +6,14 @@ describe("Twig.js Regression Tests ->", function() {
         // Define and save a template
         twig({data: '{% for note in notes %}{{note}}{% endfor %}'}).render({notes:['a', 'b', 'c']}).should.equal("abc");
     });
+
+    it("\#56 functions work inside parentheses", function() {
+        // Define and save a template
+        Twig.extendFunction('custom', function(value) {
+        	return true;
+        });
+
+        twig({data: '{% if (custom("val") and custom("val")) %}out{% endif %}'}).render({}).should.equal("out");
+    });
 });
 
