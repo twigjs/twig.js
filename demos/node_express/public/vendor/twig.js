6c4f31cbfac12266405daac6565146aa717ac23a
Morten Sjøgren <m_abs@mabs.dk>
2013-04-27 00:54:14 +0200

    Twig filter twig is suppose to escape HTML and should take a character_mask list
    This isn't actually documented in their API docs but was in their
    tests:
    trim is suppose to escape HTML.
    
    twig.js didn't implemenet: the character_mask argument.
    
    This commit fixes both

--- demos/node_express/public/vendor/twig.js
+++ demos/node_express/public/vendor/twig.js
@@ -3921,8 +3921,13 @@ var Twig = (function (Twig) {
 				return;
 			}
 
-			var str = '' + value;
-			var whitespace = ' \n\r\t\f\x0b\xa0\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u200b\u2028\u2029\u3000';
+			var str = Twig.filters.escape( '' + value ),
+				whitespace;
+			if ( params && params[0] ) {
+				whitespace = '' + params[0];
+			} else {
+				whitespace = ' \n\r\t\f\x0b\xa0\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u200b\u2028\u2029\u3000';
+			}
 			for (var i = 0; i < str.length; i++) {
 				if (whitespace.indexOf(str.charAt(i)) === -1) {
 					str = str.substring(i);
