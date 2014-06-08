19fe85e98f399f81b59457022d4e1c829e74bae9
Jeffrey Mealo <jeffreymealo@gmail.com>
2014-06-08 19:37:51 -0400

    Implemented batch filter

--- src/twig.filters.js
+++ src/twig.filters.js
@@ -507,6 +507,38 @@ var Twig = (function (Twig) {
         raw: function(value) {
             //Raw filter shim
             return value;
+        },
+        batch: function(items, params) {
+            var size = params.shift(),
+                fill = params.shift(),
+                result,
+                last,
+                missing;
+
+            if (!Twig.lib.is("Array", items)) {
+                throw new Twig.Error("batch filter expects items to be an array");
+            }
+
+            if (!Twig.lib.is("Number", size)) {
+                throw new Twig.Error("batch filter expects size to be a number");
+            }
+
+            size = Math.ceil(size);
+
+            result = Twig.lib.chunkArray(items, size);
+
+            if (fill && items.length % size != 0) {
+                last = result.pop();
+                missing = size - last.length;
+
+                while (missing--) {
+                    last.push(fill);
+                }
+
+                result.push(last);
+            }
+
+            return result;
         }
     };
 
--- src/twig.lib.js
+++ src/twig.lib.js
@@ -612,6 +612,23 @@ var Twig = (function(Twig) {
         return string.split(search).join(replace);
     };
 
+    // chunk an array (arr) into arrays of (size) items, returns an array of arrays, or an empty array on invalid input
+    Twig.lib.chunkArray = function (arr, size) {
+        var returnVal = [],
+            x = 0,
+            len = arr.length;
+
+        if (size < 1 || !Twig.lib.is("Array", arr)) {
+            return [];
+        }
+
+        while (x < len) {
+            returnVal.push(arr.slice(x, x += size));
+        }
+
+        return returnVal;
+    };
+
     return Twig;
 
 })(Twig || { });
