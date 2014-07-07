917cb734f89651c09859e2046a81926a2761a066
Evgeniy Abduzhapparov <evgeniy.abduzhapparov@foxycart.com>
2014-07-07 13:41:28 +0400

    #168 Add support of template_from_string()

--- src/twig.functions.js
+++ src/twig.functions.js
@@ -169,6 +169,11 @@ var Twig = (function (Twig) {
             }
             // Array will return element 0-index
             return object[method] || undefined;
+        },
+        template_from_string: function(template) {
+            return new Twig.Template({
+                data: template
+            });
         }
     };
 
