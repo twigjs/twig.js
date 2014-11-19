bc4d51c06c11d15076a6436959a725b4288dedf4
Nikita Groshin <nikita@groshin.ru>
2014-11-19 16:12:07 -0600

    reset build related changes

--- demos/twitter_backbone/vendor/twig.js
+++ demos/twitter_backbone/vendor/twig.js
@@ -4470,39 +4470,6 @@ var Twig = (function (Twig) {
             return whitespace.indexOf(str.charAt(0)) === -1 ? str : '';
         },
 
-        truncate: function (value, params) {
-            var length = 30,
-                preserve = false,
-                separator = '...';
-
-            value =  value + '';
-            if (params) {
-                if (params[0]) {
-                    length = params[0];
-                }
-                if (params[1]) {
-                    preserve = params[1];
-                }
-                if (params[2]) {
-                    separator = params[2];
-                }
-            }
-
-            if (value.length > length) {
-
-                if (preserve) {
-                    length = value.indexOf(' ', length);
-                    if (length === -1) {
-                        return value;
-                    }
-                }
-
-                value =  value.substr(0, length) + separator;
-            }
-
-            return value;
-        },
-
         slice: function(value, params) {
             if (value === undefined || value === null) {
                 return;
