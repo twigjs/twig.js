69d610e41b4422464859790f4498defb19eff798
Evgeniy Abduzhapparov <evgeniy.abduzhapparov@foxycart.com>
2015-01-11 16:08:55 +0300

    Support all lower-case and upper-case reserved words only
    TrUe, falSE and NUlL should not be supported.

--- src/twig.expression.js
+++ src/twig.expression.js
@@ -691,7 +691,7 @@ var Twig = (function (Twig) {
              */
             type: Twig.expression.type._null,
             // match a number
-            regex: /^null/i,
+            regex: /^(null|NULL|none|NONE)/,
             next: Twig.expression.set.operations,
             compile: function(token, stack, output) {
                 delete token.match;
@@ -732,7 +732,7 @@ var Twig = (function (Twig) {
              * Match a boolean
              */
             type: Twig.expression.type.bool,
-            regex: /^(true|false)/i,
+            regex: /^(true|TRUE|false|FALSE)/,
             next: Twig.expression.set.operations,
             compile: function(token, stack, output) {
                 token.value = (token.match[0].toLowerCase( ) === "true");
