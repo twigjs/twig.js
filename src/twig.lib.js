3b4a91f600eda83693a8c561e1ff7d40166cc52b
Evgeniy Abduzhapparov <evgeniy.abduzhapparov@foxycart.com>
2015-08-14 23:09:17 +0300

    Use latest min and max functions

--- src/twig.lib.js
+++ src/twig.lib.js
@@ -858,7 +858,7 @@ var Twig = (function(Twig) {
             n = 0,
             argv = arguments,
             argc = argv.length,
-            _obj2Array = function (obj) {
+            _obj2Array = function(obj) {
                 if (Object.prototype.toString.call(obj) === '[object Array]') {
                     return obj;
                 } else {
@@ -870,57 +870,57 @@ var Twig = (function(Twig) {
                     }
                     return ar;
                 }
-            }; //function _obj2Array
-        _compare = function (current, next) {
-            var i = 0,
-                n = 0,
-                tmp = 0,
-                nl = 0,
-                cl = 0;
+            }, //function _obj2Array
+            _compare = function(current, next) {
+                var i = 0,
+                    n = 0,
+                    tmp = 0,
+                    nl = 0,
+                    cl = 0;
 
-            if (current === next) {
-                return 0;
-            } else if (typeof current === 'object') {
-                if (typeof next === 'object') {
-                    current = _obj2Array(current);
-                    next = _obj2Array(next);
-                    cl = current.length;
-                    nl = next.length;
-                    if (nl > cl) {
-                        return 1;
-                    } else if (nl < cl) {
-                        return -1;
-                    }
-                    for (i = 0, n = cl; i < n; ++i) {
-                        tmp = _compare(current[i], next[i]);
-                        if (tmp == 1) {
+                if (current === next) {
+                    return 0;
+                } else if (typeof current === 'object') {
+                    if (typeof next === 'object') {
+                        current = _obj2Array(current);
+                        next = _obj2Array(next);
+                        cl = current.length;
+                        nl = next.length;
+                        if (nl > cl) {
                             return 1;
-                        } else if (tmp == -1) {
+                        } else if (nl < cl) {
                             return -1;
                         }
+                        for (i = 0, n = cl; i < n; ++i) {
+                            tmp = _compare(current[i], next[i]);
+                            if (tmp == 1) {
+                                return 1;
+                            } else if (tmp == -1) {
+                                return -1;
+                            }
+                        }
+                        return 0;
                     }
-                    return 0;
+                    return -1;
+                } else if (typeof next === 'object') {
+                    return 1;
+                } else if (isNaN(next) && !isNaN(current)) {
+                    if (current == 0) {
+                        return 0;
+                    }
+                    return (current < 0 ? 1 : -1);
+                } else if (isNaN(current) && !isNaN(next)) {
+                    if (next == 0) {
+                        return 0;
+                    }
+                    return (next > 0 ? 1 : -1);
                 }
-                return -1;
-            } else if (typeof next === 'object') {
-                return 1;
-            } else if (isNaN(next) && !isNaN(current)) {
-                if (current == 0) {
-                    return 0;
-                }
-                return (current < 0 ? 1 : -1);
-            } else if (isNaN(current) && !isNaN(next)) {
-                if (next == 0) {
-                    return 0;
-                }
-                return (next > 0 ? 1 : -1);
-            }
 
-            if (next == current) {
-                return 0;
-            }
-            return (next > current ? 1 : -1);
-        }; //function _compare
+                if (next == current) {
+                    return 0;
+                }
+                return (next > current ? 1 : -1);
+            }; //function _compare
         if (argc === 0) {
             throw new Error('At least one value should be passed to max()');
         } else if (argc === 1) {
@@ -969,7 +969,7 @@ var Twig = (function(Twig) {
             n = 0,
             argv = arguments,
             argc = argv.length,
-            _obj2Array = function (obj) {
+            _obj2Array = function(obj) {
                 if (Object.prototype.toString.call(obj) === '[object Array]') {
                     return obj;
                 }
@@ -980,57 +980,58 @@ var Twig = (function(Twig) {
                     }
                 }
                 return ar;
-            }; //function _obj2Array
-        _compare = function (current, next) {
-            var i = 0,
-                n = 0,
-                tmp = 0,
-                nl = 0,
-                cl = 0;
+            }, //function _obj2Array
+            _compare = function(current, next) {
+                var i = 0,
+                    n = 0,
+                    tmp = 0,
+                    nl = 0,
+                    cl = 0;
 
-            if (current === next) {
-                return 0;
-            } else if (typeof current === 'object') {
-                if (typeof next === 'object') {
-                    current = _obj2Array(current);
-                    next = _obj2Array(next);
-                    cl = current.length;
-                    nl = next.length;
-                    if (nl > cl) {
-                        return 1;
-                    } else if (nl < cl) {
-                        return -1;
-                    }
-                    for (i = 0, n = cl; i < n; ++i) {
-                        tmp = _compare(current[i], next[i]);
-                        if (tmp == 1) {
+                if (current === next) {
+                    return 0;
+                } else if (typeof current === 'object') {
+                    if (typeof next === 'object') {
+                        current = _obj2Array(current);
+                        next = _obj2Array(next);
+                        cl = current.length;
+                        nl = next.length;
+                        if (nl > cl) {
                             return 1;
-                        } else if (tmp == -1) {
+                        } else if (nl < cl) {
                             return -1;
                         }
+                        for (i = 0, n = cl; i < n; ++i) {
+                            tmp = _compare(current[i], next[i]);
+                            if (tmp == 1) {
+                                return 1;
+                            } else if (tmp == -1) {
+                                return -1;
+                            }
+                        }
+                        return 0;
                     }
-                    return 0;
+                    return -1;
+                } else if (typeof next === 'object') {
+                    return 1;
+                } else if (isNaN(next) && !isNaN(current)) {
+                    if (current == 0) {
+                        return 0;
+                    }
+                    return (current < 0 ? 1 : -1);
+                } else if (isNaN(current) && !isNaN(next)) {
+                    if (next == 0) {
+                        return 0;
+                    }
+                    return (next > 0 ? 1 : -1);
                 }
-                return -1;
-            } else if (typeof next === 'object') {
-                return 1;
-            } else if (isNaN(next) && !isNaN(current)) {
-                if (current == 0) {
-                    return 0;
-                }
-                return (current < 0 ? 1 : -1);
-            } else if (isNaN(current) && !isNaN(next)) {
-                if (next == 0) {
-                    return 0;
-                }
-                return (next > 0 ? 1 : -1);
-            }
 
-            if (next == current) {
-                return 0;
-            }
-            return (next > current ? 1 : -1);
-        }; //function _compare
+                if (next == current) {
+                    return 0;
+                }
+                return (next > current ? 1 : -1);
+            }; //function _compare
+
         if (argc === 0) {
             throw new Error('At least one value should be passed to min()');
         } else if (argc === 1) {
@@ -1039,6 +1040,7 @@ var Twig = (function(Twig) {
             } else {
                 throw new Error('Wrong parameter count for min()');
             }
+
             if (ar.length === 0) {
                 throw new Error('Array must contain at least one element for min()');
             }
@@ -1047,6 +1049,7 @@ var Twig = (function(Twig) {
         }
 
         retVal = ar[0];
+
         for (i = 1, n = ar.length; i < n; ++i) {
             if (_compare(retVal, ar[i]) == -1) {
                 retVal = ar[i];
