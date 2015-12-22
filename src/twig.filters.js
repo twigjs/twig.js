cb6bcc3d39cc0c13f9fe35cf0437640be0c4b942
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2015-12-22 11:40:11 +0100

    Filter 'sort': the compare function should return a number, not true/false
    - On Safari the test for sorting objects failed. The result of the compare
      function should be <0 if a<b, 0 if a=b, >0 if a>b. The old compare function
      returned true/false.

--- src/twig.filters.js
+++ src/twig.filters.js
@@ -87,21 +87,29 @@ var Twig = (function (Twig) {
 
                         // if a and b are comparable, we're fine :-)
                         if((value[a] > value[b]) == !(value[a] <= value[b])) {
-                            return value[a] > value[b];
+                            return value[a] > value[b] ? 1 :
+			           value[a] < value[b] ? -1 :
+				   0;
                         }
                         // if a and b can be parsed as numbers, we can compare
                         // their numeric value
                         else if(!isNaN(a1 = parseFloat(value[a])) &&
                                 !isNaN(b1 = parseFloat(value[b]))) {
-                            return a1 > b1;
+                            return a1 > b1 ? 1 :
+			           a1 < b1 ? -1 :
+				   0;
                         }
                         // if one of the values is a string, we convert the
                         // other value to string as well
                         else if(typeof value[a] == 'string') {
-                            return value[a] > value[b].toString();
+                            return value[a] > value[b].toString() ? 1 :
+                                   value[a] < value[b].toString() ? -1 :
+				   0;
                         }
                         else if(typeof value[b] == 'string') {
-                            return value[a].toString() > value[b];
+                            return value[a].toString() > value[b] ? 1 :
+                                   value[a].toString() < value[b] ? -1 :
+				   0;
                         }
                         // everything failed - return 'null' as sign, that
                         // the values are not comparable
