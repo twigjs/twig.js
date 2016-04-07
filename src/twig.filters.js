797ecbdd71ec456f25331e607ce2fc46c6369b9f
Dave Irvine <dave@daveirvine.me>
2016-04-07 08:35:51 +0100

    Remove our reimplementations of PHP functions to depend on phpjs

--- src/twig.filters.js
+++ src/twig.filters.js
@@ -284,7 +284,7 @@ module.exports = function (Twig) {
         date: function(value, params) {
             var date = Twig.functions.date(value);
             var format = params && params.length ? params[0] : 'F j, Y H:i';
-            return Twig.lib.formatDate(date, format);
+            return Twig.lib.date(format, date);
         },
 
         date_modify: function(value, params) {
