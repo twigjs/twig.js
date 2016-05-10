c270559d9aa4ecdcfb5e5bd77750f8bab63b8345
Dave Irvine <dave@daveirvine.me>
2016-05-10 15:05:48 +0100

    Add browser specific build to webpack (#363)

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,21 @@
 var webpack = require('webpack');
 
+var env = process.env.WEBPACK_ENV;
+
 module.exports = {
     entry: './src/twig.js',
-    target: 'node',
+    target: env === 'browser' ? 'web' : 'node',
     node: {
         __dirname: false,
         __filename: false,
     },
     output: {
         path: __dirname,
-        filename: 'twig.js',
+        filename: env === 'browser' ? 'twig.min.js' : 'twig.js',
         library: 'Twig',
         libraryTarget: 'umd'
-    }
+    },
+    plugins: env === 'browser' ? [
+        new webpack.optimize.UglifyJsPlugin({minimize: true})
+    ] : []
 };
