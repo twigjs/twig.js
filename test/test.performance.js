b2bde160f6a0acb1f74663f3625371fa47a5a772
Jorgen Evens <jorgen@evens.eu>
2017-06-20 23:45:49 +0200

    Add tests that checks rendering performance is acceptable.

---
+++ test/test.performance.js
@@ -0,0 +1,41 @@
+var Twig = Twig || requireUncached("../twig"),
+    twig = twig || Twig.twig;
+
+describe("Twig.js Performance Regressions ->", function() {
+
+    var template = "{{ echo_test }}\
+    {% for item in items %}\
+        <h1>{{ item.title }}</h1>\
+        {% if item.is_active %}\
+            <h3 style=\"color: green;\">Active</h3>\
+        {% else %}\
+            <h3 style=\"color: red;\">Inactive</h3>\
+        {% endif %}\
+    {% endfor %}";
+
+    it("Should not start running slower", function() {
+        this.timeout(500);
+        this.retries(3);
+        this.slow(300);
+
+        console.time('Should not start running slower ');
+        for(var i = 0; i < 1000; i++) {
+            Twig.twig({
+                data: template
+            }).render({
+                echo_test: 'Data for echo',
+                items: [
+                    {
+                        title: "This is a title",
+                        is_active: true
+                    },
+                    {
+                        title: "This is a title",
+                        is_active: false
+                    }
+                ]
+            });
+        }
+    });
+
+});
