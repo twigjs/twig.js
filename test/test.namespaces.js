612fa368a2ecb8d5ac093d88b63d0489653a9227
Rene Bosch <boschr@home.nl>
2018-05-25 22:09:38 +0200

    improve namespaces: support non namespaces path together with namespa… (#556)
    * improve namespaces: support non namespaces path together with namespaces configuration
    Fixes #555
    
    * add spec: should support non-namespaced includes with namespaces configured

--- test/test.namespaces.js
+++ test/test.namespaces.js
@@ -15,7 +15,7 @@ describe("Twig.js Namespaces ->", function() {
 
 				done();
             }
-    	});        
+    	});
     });
 
     it("should support namespaces defined with @", function(done) {
@@ -31,8 +31,22 @@ describe("Twig.js Namespaces ->", function() {
 
 				done();
             }
-    	});        
+    	});
+	});
+
+	it("should support non-namespaced includes with namespaces configured", function(done) {
+    	twig({
+			namespaces: { 'test': 'test/templates/namespaces/' },
+			path: 'test/templates/namespaces_without_namespace.twig',
+			load: function(template) {
+				// Render the template
+				template.render({
+				    test: "yes",
+				    flag: true
+				}).should.equal("namespaces\nnamespaces");
+
+				done();
+            }
+    	});
     });
-
-
 });
