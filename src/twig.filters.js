48fcdada65a7a8c112af3ccc9deb8db252144c5d
toptalo <spam.vitek@gmail.com>
2018-06-19 18:18:21 +0300

    Fix date escape sequences

--- src/twig.filters.js
+++ src/twig.filters.js
@@ -284,10 +284,11 @@ module.exports = function (Twig) {
 
             return obj;
         },
+
         date: function(value, params) {
             var date = Twig.functions.date(value);
             var format = params && params.length ? params[0] : 'F j, Y H:i';
-            return Twig.lib.date(format, date);
+            return Twig.lib.date(format.replace(/\\\\/g,'\\'), date);
         },
 
         date_modify: function(value, params) {
