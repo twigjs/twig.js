b913c2f78857449d8923bd15756b7f3fd770df2a
Howard Li <bighorn@pursuitofchallenge.com>
2019-05-16 21:42:57 -0700

    Delint twig.loader.ajax.js

--- src/twig.loader.ajax.js
+++ src/twig.loader.ajax.js
@@ -2,23 +2,22 @@ module.exports = function (Twig) {
     'use strict';
 
     Twig.Templates.registerLoader('ajax', function (location, params, callback, errorCallback) {
-        var template,
-            xmlhttp,
-            precompiled = params.precompiled,
-            parser = this.parsers[params.parser] || this.parser.twig;
+        let template;
+        const {precompiled} = params;
+        const parser = this.parsers[params.parser] || this.parser.twig;
 
-        if (typeof XMLHttpRequest === "undefined") {
+        if (typeof XMLHttpRequest === 'undefined') {
             throw new Twig.Error('Unsupported platform: Unable to do ajax requests ' +
                                  'because there is no "XMLHTTPRequest" implementation');
         }
 
-        xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            var data = null;
+        const xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function () {
+            let data = null;
 
-            if(xmlhttp.readyState === 4) {
-                if (xmlhttp.status === 200 || (window.cordova && xmlhttp.status == 0)) {
-                    Twig.log.debug("Got template ", xmlhttp.responseText);
+            if (xmlhttp.readyState === 4) {
+                if (xmlhttp.status === 200 || (window.cordova && xmlhttp.status === 0)) {
+                    Twig.log.debug('Got template ', xmlhttp.responseText);
 
                     if (precompiled === true) {
                         data = JSON.parse(xmlhttp.responseText);
@@ -34,22 +33,20 @@ module.exports = function (Twig) {
                     if (typeof callback === 'function') {
                         callback(template);
                     }
-                } else {
-                    if (typeof errorCallback === 'function') {
-                        errorCallback(xmlhttp);
-                    }
+                } else if (typeof errorCallback === 'function') {
+                    errorCallback(xmlhttp);
                 }
             }
         };
-        xmlhttp.open("GET", location, !!params.async);
+
+        xmlhttp.open('GET', location, Boolean(params.async));
         xmlhttp.send();
 
         if (params.async) {
             // TODO: return deferred promise
             return true;
-        } else {
-            return template;
         }
-    });
 
+        return template;
+    });
 };
