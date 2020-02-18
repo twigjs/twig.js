5ac20b0e983045d75cac8828aba9491b6098de13
Rob Loach <robloach@gmail.com>
2020-02-17 23:23:14 -0500

    Update twitter_backbone and add Browser example

--- demos/twitter_backbone/app.js
+++ demos/twitter_backbone/app.js
@@ -10,28 +10,28 @@ http.createServer(function(request, response) {
   var uri = url.parse(request.url).pathname
     , filename = path.join(__dirname, uri);
 
-  path.exists(filename, function(exists) {
-    if(!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found\n");
+  if (!fs.existsSync(filename)) {
+    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.write("404 Not Found\n");
+    response.end();
+    return;
+  }
+
+  if (fs.statSync(filename).isDirectory()) {
+      filename += '/index.html';
+  }
+
+  fs.readFile(filename, "binary", function(err, file) {
+    if(err) {
+      response.writeHead(500, {"Content-Type": "text/plain"});
+      response.write(err + "\n");
       response.end();
       return;
     }
 
-if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-
-    fs.readFile(filename, "binary", function(err, file) {
-      if(err) {
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
-        response.end();
-        return;
-      }
-
-      response.writeHead(200);
-      response.write(file, "binary");
-      response.end();
-    });
+    response.writeHead(200);
+    response.write(file, "binary");
+    response.end();
   });
 }).listen(parseInt(port, 10), host);
 
