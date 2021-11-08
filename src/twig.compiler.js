506af5ee8dade96cc8af8a9dbc24de50e211adce
dmytro-lt <dmytro@langivi.technology>
2021-11-08 14:15:07 +0200

    Change to classes

--- src/twig.compiler.js
+++ src/twig.compiler.js
@@ -10,40 +10,85 @@ export default function (Twig) {
     };
 
     // Compile a Twig Template to output.
-    Twig.compiler.compile = function (template, options) {
-        // Get tokens
-        const tokens = JSON.stringify(template.tokens);
-        const {id} = template;
-        let output = null;
+    // Twig.compiler.compile = function (template, options) {
+    //     // Get tokens
+    //     const tokens = JSON.stringify(template.tokens);
+    //     const {id} = template;
+    //     let output = null;
 
-        if (options.module) {
-            if (Twig.compiler.module[options.module] === undefined) {
-                throw new Twig.Error('Unable to find module type ' + options.module);
-            }
+    //     if (options.module) {
+    //         if (Twig.compiler.module[options.module] === undefined) {
+    //             throw new Twig.Error('Unable to find module type ' + options.module);
+    //         }
 
-            output = Twig.compiler.module[options.module](id, tokens, options.twig);
+    //         output = Twig.compiler.module[options.module](id, tokens, options.twig);
+    //     } else {
+    //         output = Twig.compiler.wrap(id, tokens);
+    //     }
+
+    //     return output;
+    // };
+    class TwigCompilerComplite {
+
+        Twig;
+        constructor(Twig) {
+            this.Twig = Twig;
+        }
+
+        compile(template, options) {
+            const tokens = JSON.stringify(template.tokens);
+            const {id} = template;
+            let output = null;
+
+            if (options.module) {
+                if (Twig.compiler.module[options.module] === undefined) {
+                    throw new Twig.Error('Unable to find module type ' + options.module);
+                }
+
+                output = Twig.compiler.module[options.module](id, tokens, options.twig);
         } else {
             output = Twig.compiler.wrap(id, tokens);
         }
 
         return output;
-    };
 
-    Twig.compiler.module = {
+        }
+    }
+
+    // Twig.compiler.module = {
+    //     amd(id, tokens, pathToTwig) {
+    //         return 'define(["' + pathToTwig + '"], function (Twig) {\n\tvar twig, templates;\ntwig = Twig.twig;\ntemplates = ' + Twig.compiler.wrap(id, tokens) + '\n\treturn templates;\n});';
+    //     },
+    //     node(id, tokens) {
+    //         return 'var twig = require("twig").twig;\nexports.template = ' + Twig.compiler.wrap(id, tokens);
+    //     },
+    //     cjs2(id, tokens, pathToTwig) {
+    //         return 'module.declare([{ twig: "' + pathToTwig + '" }], function (require, exports, module) {\n\tvar twig = require("twig").twig;\n\texports.template = ' + Twig.compiler.wrap(id, tokens) + '\n});';
+    //     }
+    // };
+
+    class TwigCompilerModule {
         amd(id, tokens, pathToTwig) {
             return 'define(["' + pathToTwig + '"], function (Twig) {\n\tvar twig, templates;\ntwig = Twig.twig;\ntemplates = ' + Twig.compiler.wrap(id, tokens) + '\n\treturn templates;\n});';
-        },
+        };
         node(id, tokens) {
             return 'var twig = require("twig").twig;\nexports.template = ' + Twig.compiler.wrap(id, tokens);
-        },
+        };
         cjs2(id, tokens, pathToTwig) {
             return 'module.declare([{ twig: "' + pathToTwig + '" }], function (require, exports, module) {\n\tvar twig = require("twig").twig;\n\texports.template = ' + Twig.compiler.wrap(id, tokens) + '\n});';
         }
-    };
+    }
 
-    Twig.compiler.wrap = function (id, tokens) {
-        return 'twig({id:"' + id.replace('"', '\\"') + '", data:' + tokens + ', precompiled: true});\n';
-    };
+    class TwigCompilerWrap {
+        fn(id, tokens) {
+            return 'twig({id:"' + id.replace('"', '\\"') + '", data:' + tokens + ', precompiled: true});\n';
+        }
 
-    return Twig;
+        // return Twig
+    }
+    // Twig.compiler.wrap = function (id, tokens) {
+    //     return 'twig({id:"' + id.replace('"', '\\"') + '", data:' + tokens + ', precompiled: true});\n';
+    // };
+
+    // return Twig;
 };
