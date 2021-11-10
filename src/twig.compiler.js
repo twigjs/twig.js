57c667a9c830cb243d2ba0f80f745f31749bd244
dmytro-lt <dmytro@langivi.technology>
2021-11-10 10:21:32 +0200

    Added changes export, fields, methods

--- src/twig.compiler.js
+++ src/twig.compiler.js
@@ -1,44 +1,33 @@
 // ## twig.compiler.js
 //
 // This file handles compiling templates into JS
-export default function (Twig) {
-    /**
-     * Namespace for compilation.
-     */
-    Twig.compiler = {
+export default class TwigCompiler {
+
+    constructor() {
         module: {}
-    };
-    class TwigCompiler {
-        compile(template, options) {
-            const tokens = JSON.stringify(template.tokens);
-            const {id} = template;
-            let output = null;
-
-            if (options.module) {
-                if (Twig.compiler.module[options.module] === undefined) {
-                    throw new Twig.Error('Unable to find module type ' + options.module);
-                }
-
-                output = Twig.compiler.module[options.module](id, tokens, options.twig);
-            } else {
-                output = Twig.compiler.wrap(id, tokens);
-            }
-            return output;
-        } 
-        module() {
-            amd: (id, tokens, pathToTwig) => {
-                return 'define(["' + pathToTwig + '"], function (Twig) {\n\tvar twig, templates;\ntwig = Twig.twig;\ntemplates = ' + Twig.compiler.wrap(id, tokens) + '\n\treturn templates;\n});';
-            }
-            node: (id, tokens) => {
-                return 'var twig = require("twig").twig;\nexports.template = ' + Twig.compiler.wrap(id, tokens);
-            }
-            cjs2: (id, tokens, pathToTwig) => {
-                return 'module.declare([{ twig: "' + pathToTwig + '" }], function (require, exports, module) {\n\tvar twig = require("twig").twig;\n\texports.template = ' + Twig.compiler.wrap(id, tokens) + '\n});';
-            }
-        }
-        wrap() {
-            fn: (id, tokens) => 'twig({id:"' + id.replace('"', '\\"') + '", data:' + tokens + ', precompiled: true});\n';
-        }
     }
-    return Twig
+
+    compile(template, options) {
+        // Get tokens
+        const tokens = JSON.stringify(template.tokens);
+        const {id} = template;
+        let output = null;
+
+        if (options.module) {
+            if (Twig.compiler.module[options.module] === undefined) {
+                throw new Twig.Error('Unable to find module type ' + options.module);
+            }
+
+            output = Twig.compiler.module[options.module](id, tokens, options.twig);
+        } else {
+            output = Twig.compiler.wrap(id, tokens);
+        }
+        return output;
+    }
+
+    module: {}
+
+    wrap(id, tokens) {
+        return 'twig({id:"' + id.replace('"', '\\"') + '", data:' + tokens + ', precompiled: true});\n';
+    }
 }
