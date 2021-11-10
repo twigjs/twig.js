8f5d363f1442c71d120405081b1b160002c4a5ba
dmytro-lt <dmytro@langivi.technology>
2021-11-10 12:11:27 +0200

    Changed module and other

--- src/twig.compiler.js
+++ src/twig.compiler.js
@@ -14,18 +14,24 @@ export default class TwigCompiler {
         let output = null;
 
         if (options.module) {
-            if (Twig.compiler.module[options.module] === undefined) {
-                throw new Twig.Error('Unable to find module type ' + options.module);
+            if (this.Twig.compiler.module[options.module] === undefined) {
+                throw new this.Twig.Error('Unable to find module type ' + options.module);
             }
 
-            output = Twig.compiler.module[options.module](id, tokens, options.twig);
+            output = this.module[options.module](id, tokens, options.twig);
         } else {
-            output = Twig.compiler.wrap(id, tokens);
+            output = this.wrap(id, tokens);
         }
         return output;
     }
 
-    module: {}
+    module: {
+        amd: (id, tokens, pathToTwig) => 'define(["' + pathToTwig + '"], function (Twig) {\n\tvar twig, templates;\ntwig = Twig.twig;\ntemplates = ' + this.Twig.compiler.wrap(id, tokens) + '\n\treturn templates;\n});';
+
+        node: (id, tokens) => 'var twig = require("twig").twig;\nexports.template = ' + this.Twig.compiler.wrap(id, tokens);
+
+        cjs2: (id, tokens, pathToTwig) => 'module.declare([{ twig: "' + pathToTwig + '" }], function (require, exports, module) {\n\tvar twig = require("twig").twig;\n\texports.template = ' + this.Twig.compiler.wrap(id, tokens) + '\n});';
+    }
 
     wrap(id, tokens) {
         return 'twig({id:"' + id.replace('"', '\\"') + '", data:' + tokens + ', precompiled: true});\n';
