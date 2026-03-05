845f205356fbc3e9ec23f5a4b4b6038ff2d58239
Thomas Hohn <tho@gyldendal.dk>
2026-03-05 11:54:22 +0100

    Update locutus to ^3

--- src/twig.filters.js
+++ src/twig.filters.js
@@ -389,7 +389,7 @@ module.exports = function (Twig) {
         },
 
         format(value, params) {
-            if (value === undefined || value === null) {
+            if (value === undefined || value === null || value === '') {
                 return;
             }
 
